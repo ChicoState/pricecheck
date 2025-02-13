aa513bb825ace4bf57afddfb19b6082a8aa63278
annasehgal <annasehgal05@gmail.com>
2025-02-12 17:53:38 -0800

    added button

--- popup.js
+++ popup.js
@@ -1,1 +1,5 @@
-// fetch results and display
+console.log("This is a popup!");
+
+document.getElementById("checkPrice").addEventListener("click", () => {
+  console.log("Price check initiated.");
+});
