bdebf78ede566674a77d6b01d25faf6e6d855738
annasehgal <annasehgal05@gmail.com>
2025-03-10 03:21:08 -0700

    Untrack sensitive files and virtual environment files

--- my_django_project/django_pricechecker/django_pricechecker/urls.py
+++ my_django_project/django_pricechecker/django_pricechecker/urls.py
@@ -1,3 +1,23 @@
+# # from django.contrib import admin
+# # from django.urls import path
+# # from user_app.views import index, login_page, dashboard
+
+# # urlpatterns = [
+# #     path('admin/', admin.site.urls),
+# #     path('', index, name='index'),          # if you want index for "/"
+# #     path('login/', login_page, name='login'),
+# #     path('dashboard/', dashboard, name='dashboard'),
+# # ]
+
+# from django.contrib import admin
+# from django.urls import path
+# from user_app import views
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', views.login_page, name='login'),
+#     path('dashboard/', views.dashboard, name='dashboard'),
+# ]
 # from django.contrib import admin
 # from django.urls import path
 # from user_app.views import index, login_page, dashboard
